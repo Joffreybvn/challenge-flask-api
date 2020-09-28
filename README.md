c9f46bdd734a960e2ae3897aaef691c92ff72388
Joffrey <joffreybvn@gmail.com>
2020-09-28 11:25:54 +0200

    Exercices "How to flask" completed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Challenge: Flask AP
+# Challenge: Flask API
 [![Generic badge](https://img.shields.io/badge/Python-3.8-blue.svg)](https://www.python.org/downloads/release/python-380/)
 
 Author: [Joffrey Bienvenu](https://github.com/Joffreybvn)
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return 'Hello World'
+
+
+# Get example method
+@app.route('/get', methods=['GET'])
+def user():
+    if request.method == 'GET':
+        return f"Hello get !"
+
+
+# Post example method
+@app.route('/post', methods=['POST'])
+def post():
+    if request.method == 'POST':
+        return f"Hello post !"
+
+
+# Multiply by 2 - with Get, example method
+@app.route('/multiply/<int:number>')
+def multiply(number):
+    return f"Result: {number * 2}"
 
 
 if __name__ == '__main__':
