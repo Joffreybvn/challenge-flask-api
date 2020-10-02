caf5ceab411db518cfbee378ac263cf3fcef9349
Joffrey <joffreybvn@gmail.com>
2020-10-02 14:02:31 +0200

    API port ready for production

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 # https://flask-restx.readthedocs.io/en/latest/swagger.html
 # Imports
+import os
 from api.app import create_app
 
+port = int(os.environ.get("PORT", 5000))
 api = create_app()
 
 
 # Run the app
 if __name__ == '__main__':
-    api.run(port=5000, debug=False, threaded=True)
+    api.run('0.0.0.0', port=port, debug=False, threaded=True)
